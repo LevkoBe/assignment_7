ece4b2310eb0337227899b263eeb6da199a903d1
Denis Pasichnyk <119937568+DenisPasichnyk@users.noreply.github.com>
2022-12-10 17:29:01 +0200

    add function which check country and print name of athlet

--- main.py
+++ main.py
@@ -9,11 +9,22 @@ print(args.medals)
 
 with args.infile as file:   
     next_line = file.readline()   
-    store = [next_line]   
+    store = []   
     while next_line:       
         # do stuff with line       
-        next_line = file.readline()       
-        store.append(next_line)  
+        next_line = file.readline()
+        data = [next_line.split('\t')]  
+
+        amount_of_people = 0
+
+        if args.medals[0] in data and args.medals[1] in data:
+            if amount_of_people < 10 and ('Gold\n' in data or 'Silver\n' in data or 'Bronze\n' in data):
+                store += f'{data[1]}, {data[-1]}\n'
+                amount_of_people += 1
         
+
+
 if args.output is not None:  
     args.output.writelines(f'{store[0]}\n')
+
+print(store)
