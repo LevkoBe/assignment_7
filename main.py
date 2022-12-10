2c531a86d6bb4b5cdfa4e8e878e855490acb7fd2
Levko <lbeniakh@kse.org.ua>
2022-12-10 17:59:43 +0200

    deleted square brackets

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import argparse  
+import argparse
+
 parser = argparse.ArgumentParser() 
 parser.add_argument('infile', type=argparse.FileType('r')) 
 parser.add_argument('-medals', dest='medals', nargs=2, required=True) 
@@ -13,16 +14,13 @@ with args.infile as file:
     while next_line:       
         # do stuff with line       
         next_line = file.readline()
-        data = [next_line.split('\t')]  
-
+        data = next_line.split('\t')
         amount_of_people = 0
-
         if args.medals[0] in data and args.medals[1] in data:
             if amount_of_people < 10 and ('Gold\n' in data or 'Silver\n' in data or 'Bronze\n' in data):
                 store += f'{data[1]}, {data[-1]}\n'
                 amount_of_people += 1
         
-
 
 if args.output is not None:  
     args.output.writelines(f'{store[0]}\n')
