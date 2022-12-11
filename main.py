2216de46f4494f78ade68346715a95e806a37127
Levko <lbeniakh@kse.org.ua>
2022-12-11 19:33:06 +0200

    Just reformatted the file and added Athletes

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 import argparse
 
-parser = argparse.ArgumentParser() 
-parser.add_argument('infile', type=argparse.FileType('r')) 
-parser.add_argument('-medals', dest='medals', nargs=2, required=True) 
-parser.add_argument('-output', '--output', type=argparse.FileType('w', encoding='UTF-8'))  
+parser = argparse.ArgumentParser()
+parser.add_argument('infile', type=argparse.FileType('r'))
+parser.add_argument('-medals', nargs=2, required=True)
+parser.add_argument('-output', '--output', type=argparse.FileType('w', encoding='UTF-8'))
 
-args = parser.parse_args() 
-print(args.medals)    
+args = parser.parse_args()
+print(args.medals)
 
-
-
-with args.infile as file:   
-    next_line = file.readline()   
+with args.infile as file:
+    next_line = file.readline()
     store = ''
-     
+
     amount_of_gold = 0
-    amount_of_silver =0
+    amount_of_silver = 0
     amount_of_bronze = 0
 
     amount_of_people = 0
-    
 
-    while next_line:       
-        # do stuff with line       
+    while next_line:
         next_line = file.readline()
         data = next_line.split('\t')
-        
-        
+
         if args.medals[0] in data and args.medals[1] in data:
             if amount_of_people < 10 and ('Gold\n' in data or 'Silver\n' in data or 'Bronze\n' in data):
                 store += f'{data[1]}, {data[-1]}'
@@ -35,13 +30,13 @@ with args.infile as file:
             if 'Gold\n' in data:
                 amount_of_gold += 1
             elif 'Silver\n' in data:
-                amount_of_silver += 1                       
+                amount_of_silver += 1
             elif 'Bronze\n' in data:
                 amount_of_bronze += 1
-                               
-        
-if args.output is not None:  
-    args.output.writelines(f'{store}{amount_of_gold}\n{amount_of_silver}\n{amount_of_bronze}\n')
-print(store)
 
-print (amount_of_gold , amount_of_silver , amount_of_bronze)
+if args.output is not None:
+    args.output.writelines(f'{store}\n{args.medals[0]}, year {args.medals[1]}.\n\n'
+                           f'Gold: {amount_of_gold};\nsilver: {amount_of_silver};\nbronze: {amount_of_bronze}.\n')
+
+print(f'{store}\n{args.medals[0]}, year {args.medals[1]}.\n\n'
+      f'Gold: {amount_of_gold};\nsilver: {amount_of_silver};\nbronze: {amount_of_bronze}.\n')
