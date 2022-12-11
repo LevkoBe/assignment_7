4ba5931f9428b46b50aba6bdd924094d01e42dd2
Levko <lbeniakh@kse.org.ua>
2022-12-11 22:33:59 +0200

    Added command -overall

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ parser.add_argument('infile', type=argparse.FileType('r'))
 parser.add_argument('-medals', nargs=2, required=True)
 parser.add_argument('-output', '--output', type=argparse.FileType('w', encoding='UTF-8'))
 parser.add_argument('-total', type=str)
+parser.add_argument('-overall', nargs="*")
 
 args = parser.parse_args()
 
@@ -19,10 +20,20 @@ with args.infile as file:
     amount_of_people = 0
 
     countries = {}
+    if args.overall:
+        o_countries = [{} for x in args.overall]
 
     while next_line:
         next_line = file.readline()
         data = next_line.split('\t')
+
+        if args.overall and data[-1] != 'NA\n':
+            for i, country in enumerate(args.overall):
+                if country in data:
+                    if data[9] not in o_countries[i]:
+                        o_countries[i][data[9]] = 1
+                    else:
+                        o_countries[i][data[9]] += 1
 
         if args.total and args.total in data and data[-1] != 'NA\n':
             if data[6] not in countries:
@@ -51,9 +62,26 @@ all_countries = f'In {args.total} such countries won medals:\n'
 for country in countries:
     all_countries += f'{country} - {countries[country][0]} - {countries[country][1]} - {countries[country][2]}\n'
 
-if args.output is not None:
-    args.output.writelines(f'{store}\n{args.medals[0]}, year {args.medals[1]}.\n\n{all_countries}\n\n'
-                           f'Gold: {amount_of_gold};\nsilver: {amount_of_silver};\nbronze: {amount_of_bronze}.\n\n{all_countries}')
 
-print(f'{store}\n{args.medals[0]}, year {args.medals[1]}.\n\n'
-      f'Gold: {amount_of_gold};\nsilver: {amount_of_silver};\nbronze: {amount_of_bronze}.\n\n{all_countries}')
+all_text = f'{store}\n{args.medals[0]}, year {args.medals[1]}.\n\n'\
+               f'Gold: {amount_of_gold};\nsilver: {amount_of_silver};\nbronze: {amount_of_bronze}.'
+if args.total:
+    all_text += f'\n\n{all_countries}'
+if args.overall:
+    overall_countries = ''
+    for i in range(len(o_countries)):
+        values = [int(o_countries[i][x]) for x in o_countries[i]]
+        numbers_of_medals = [int(x) for x in o_countries[i].values()]
+        max_value = max(numbers_of_medals)
+        years_of_medals = [x for x in o_countries[i].keys()]
+        year = years_of_medals[numbers_of_medals.index(max_value)]
+        overall_countries += f'{args.overall[i]} - year {year} - {max_value} medals\n'
+
+    all_text += f'\n\n{overall_countries}'
+
+
+if args.output is not None:
+    args.output.writelines(all_text)
+
+print(all_text)
+# python main.py "oa.tsv" -medals UKR 2004 -total 2000 -overall USA UKR -output "new"
