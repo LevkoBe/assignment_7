c58dcae6dd8853942b48e3047308e3176ad24d01
Denis Pasichnyk <119937568+DenisPasichnyk@users.noreply.github.com>
2022-12-11 16:33:01 +0200

    add amount of medals, and writing in file by -output

--- main.py
+++ main.py
@@ -12,9 +12,14 @@ print(args.medals)
 
 with args.infile as file:   
     next_line = file.readline()   
-    store = ''   
+    store = ''
+     
+    amount_of_gold = 0
+    amount_of_silver =0
+    amount_of_bronze = 0
 
     amount_of_people = 0
+    
 
     while next_line:       
         # do stuff with line       
@@ -26,9 +31,17 @@ with args.infile as file:
             if amount_of_people < 10 and ('Gold\n' in data or 'Silver\n' in data or 'Bronze\n' in data):
                 store += f'{data[1]}, {data[-1]}'
                 amount_of_people += 1
+
+            if 'Gold\n' in data:
+                amount_of_gold += 1
+            elif 'Silver\n' in data:
+                amount_of_silver += 1                       
+            elif 'Bronze\n' in data:
+                amount_of_bronze += 1
+                               
         
-
 if args.output is not None:  
-    args.output.writelines(f'{store[0]}\n')
-
+    args.output.writelines(f'{store}{amount_of_gold}\n{amount_of_silver}\n{amount_of_bronze}\n')
 print(store)
+
+print (amount_of_gold , amount_of_silver , amount_of_bronze)
