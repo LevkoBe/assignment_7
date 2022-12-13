e21f14749817597bf97d01eaad30d13e8adea08f
Denis Pasichnyk <119937568+DenisPasichnyk@users.noreply.github.com>
2022-12-13 18:53:37 +0200

    add function which count value of medals in different place

--- main.py
+++ main.py
@@ -10,6 +10,17 @@ parser.add_argument('-interactive', action='store_true')
 
 args = parser.parse_args()
 
+def value_of_medals(line,gold,silver,bronze):
+
+    if 'Gold\n' in line:
+        gold += 1
+    elif 'Silver\n' in line:
+        silver += 1
+    elif 'Bronze\n' in line:
+        bronze += 1   
+
+    return gold, silver, bronze 
+
 
 def interactive_mode():
     current_country = input("Enter country: ")
@@ -31,22 +42,23 @@ def interactive_mode():
                         years[current_year] = [0, 0, 0, 0]
                     if current_data[-1] != 'NA\n':
                         years[current_year][3] += 1
-                        if 'Gold\n' in current_data:
-                            years[current_year][0] += 1
-                        elif 'Silver\n' in current_data:
-                            years[current_year][1] += 1
-                        elif 'Bronze\n' in current_data:
-                            years[current_year][2] += 1
+                        
+                        years[current_year][0],years[current_year][1],years[current_year][2] \
+                        = value_of_medals(current_data,years[current_year][0],years[current_year][1],years[current_year][2])   
+
                     if int(current_year) < first_time:
                         first_time = int(current_year)
                         place = current_data[-4]
+
             all_years = [x for x in years.keys()]
             all_medals = [x[3] for x in years.values()]
             the_best = all_medals.index(max(all_medals))
             the_worst = all_medals.index(min(all_medals))
+
             print(f"The first participation in Olympiad was in {first_time} year, in {place}.")
             print(f"The most successful participation was in {all_years[the_best]} with {all_medals[the_best]} medals.")
             print(f"The least successful participation was in {all_years[the_worst]} with {all_medals[the_worst]} medals.")
+
             for the_year in years:
                 print(f"In {the_year} was won {years[the_year][0]} gold, "
                       f"{years[the_year][1]} silver and {years[the_year][2]} bronze medals.")
@@ -83,24 +95,16 @@ with open(args.infile, 'r') as file:
             if data[6] not in countries:
                 countries[data[6]] = [0, 0, 0]
 
-            if 'Gold\n' in data:
-                countries[data[6]][0] += 1
-            elif 'Silver\n' in data:
-                countries[data[6]][1] += 1
-            elif 'Bronze\n' in data:
-                countries[data[6]][2] += 1
+            countries[data[6]][0],countries[data[6]][1],countries[data[6]][2] \
+            =value_of_medals(data,countries[data[6]][0],countries[data[6]][1],countries[data[6]][2])
 
         if args.medals[0] in data and args.medals[1] in data:
             if amount_of_people < 10 and ('Gold\n' in data or 'Silver\n' in data or 'Bronze\n' in data):
                 store += f'{data[1]}, {data[-1]}'
                 amount_of_people += 1
 
-            if 'Gold\n' in data:
-                amount_of_gold += 1
-            elif 'Silver\n' in data:
-                amount_of_silver += 1
-            elif 'Bronze\n' in data:
-                amount_of_bronze += 1
+            amount_of_gold, amount_of_silver, amount_of_bronze \
+            =value_of_medals(data,amount_of_gold, amount_of_silver, amount_of_bronze)           
 
 all_countries = f'In {args.total} such countries won medals:\n'
 for country in countries:
@@ -131,4 +135,4 @@ print(all_text)
 
 if args.interactive:
     interactive_mode()
-# python main.py "oa.tsv" -medals UKR 2004 -total 2000 -overall USA UKR -output "new"
+# python main.py "oa.tsv" -medals UKR 2004 -total 2000 -overall USA UKR -output "new" -interactive
