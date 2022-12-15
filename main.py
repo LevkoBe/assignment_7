2f7d4a71101ccf5af4512ea30d672f21dc42383f
Levko <lbeniakh@kse.org.ua>
2022-12-15 12:09:26 +0200

    Deleted 122 line, reformatted file

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ parser.add_argument('-interactive', action='store_true')
 
 args = parser.parse_args()
 
-def value_of_medals(line,gold,silver,bronze):
 
+def value_of_medals(line, gold, silver, bronze):
     if 'Gold\n' in line:
         gold += 1
     elif 'Silver\n' in line:
         silver += 1
     elif 'Bronze\n' in line:
-        bronze += 1   
+        bronze += 1
 
-    return gold, silver, bronze 
+    return gold, silver, bronze
 
 
 def interactive_mode():
@@ -42,9 +42,10 @@ def interactive_mode():
                         years[current_year] = [0, 0, 0, 0]
                     if current_data[-1] != 'NA\n':
                         years[current_year][3] += 1
-                        
-                        years[current_year][0],years[current_year][1],years[current_year][2] \
-                        = value_of_medals(current_data,years[current_year][0],years[current_year][1],years[current_year][2])   
+
+                        years[current_year][0], years[current_year][1], years[current_year][2] \
+                            = value_of_medals(current_data, years[current_year][0], years[current_year][1],
+                                              years[current_year][2])
 
                     if int(current_year) < first_time:
                         first_time = int(current_year)
@@ -57,7 +58,8 @@ def interactive_mode():
 
             print(f"The first participation in Olympiad was in {first_time} year, in {place}.")
             print(f"The most successful participation was in {all_years[the_best]} with {all_medals[the_best]} medals.")
-            print(f"The least successful participation was in {all_years[the_worst]} with {all_medals[the_worst]} medals.")
+            print(
+                f"The least successful participation was in {all_years[the_worst]} with {all_medals[the_worst]} medals.")
 
             for the_year in years:
                 print(f"In {the_year} was won {years[the_year][0]} gold, "
@@ -95,8 +97,8 @@ with open(args.infile, 'r') as file:
             if data[6] not in countries:
                 countries[data[6]] = [0, 0, 0]
 
-            countries[data[6]][0],countries[data[6]][1],countries[data[6]][2] \
-            =value_of_medals(data,countries[data[6]][0],countries[data[6]][1],countries[data[6]][2])
+            countries[data[6]][0], countries[data[6]][1], countries[data[6]][2] \
+                = value_of_medals(data, countries[data[6]][0], countries[data[6]][1], countries[data[6]][2])
 
         if args.medals[0] in data and args.medals[1] in data:
             if amount_of_people < 10 and ('Gold\n' in data or 'Silver\n' in data or 'Bronze\n' in data):
@@ -104,21 +106,19 @@ with open(args.infile, 'r') as file:
                 amount_of_people += 1
 
             amount_of_gold, amount_of_silver, amount_of_bronze \
-            =value_of_medals(data,amount_of_gold, amount_of_silver, amount_of_bronze)           
+                = value_of_medals(data, amount_of_gold, amount_of_silver, amount_of_bronze)
 
 all_countries = f'In {args.total} such countries won medals:\n'
 for country in countries:
     all_countries += f'{country} - {countries[country][0]} - {countries[country][1]} - {countries[country][2]}\n'
 
-
-all_text = f'{store}\n{args.medals[0]}, year {args.medals[1]}.\n\n'\
-               f'Gold: {amount_of_gold};\nsilver: {amount_of_silver};\nbronze: {amount_of_bronze}.'
+all_text = f'{store}\n{args.medals[0]}, year {args.medals[1]}.\n\n' \
+           f'Gold: {amount_of_gold};\nsilver: {amount_of_silver};\nbronze: {amount_of_bronze}.'
 if args.total:
     all_text += f'\n\n{all_countries}'
 if args.overall:
     overall_countries = ''
     for i in range(len(o_countries)):
-        values = [int(o_countries[i][x]) for x in o_countries[i]]
         numbers_of_medals = [int(x) for x in o_countries[i].values()]
         max_value = max(numbers_of_medals)
         years_of_medals = [x for x in o_countries[i].keys()]
@@ -126,7 +126,6 @@ if args.overall:
         overall_countries += f'{args.overall[i]} - year {year} - {max_value} medals\n'
 
     all_text += f'\n\n{overall_countries}'
-
 
 if args.output is not None:
     args.output.writelines(all_text)
@@ -136,3 +135,4 @@ print(all_text)
 if args.interactive:
     interactive_mode()
 # python main.py "oa.tsv" -medals UKR 2004 -total 2000 -overall USA UKR -output "new" -interactive
+# values = [int(o_countries[i][x]) for x in o_countries[i]]
